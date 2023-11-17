af8a24c0cc496018074b87b4b56cdcba4c55a34c
SylvieHoareau <64300825+SylvieHoareau@users.noreply.github.com>
2023-11-17 09:07:28 +0400

    C - NPM

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 import name from './utils.js'
 import { getNotes } from './note.js'
+import validator from 'validator'
 
 console.log('Hello Node JS')
 console.log(name)
 console.log(getNotes())
 
+// Test du validator
+let email = "test@example.com"
+if (validator.isEmail(email)) {
+    console.log('Cet email est valide')
+} else {
+    console.log('Cet email n\est pas valide')
+}
+
+// Utilisation de chalk
+console.log(chalk.green.bold.inverse('Succes'))
