c316647bc499cd8fd29bbea5e7c48e5f02b06bb3
SylvieHoareau <64300825+SylvieHoareau@users.noreply.github.com>
2023-11-17 09:36:34 +0400

    D - Lancer un script avec des arguments

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 import name from './utils.js'
 import { getNotes } from './note.js'
 import validator from 'validator'
+import chalk from 'chalk'
+import yargs from 'yargs'
 
 console.log('Hello Node JS')
 console.log(name)
@@ -16,3 +18,50 @@ if (validator.isEmail(email)) {
 
 // Utilisation de chalk
 console.log(chalk.green.bold.inverse('Succes'))
+
+// Script avec arguments
+const command = process.argv[2]
+
+if(command === 'add') {
+    console.log('Adding note')
+} else if (command === 'remove') {
+    console.log('Removing note')
+}
+
+yargs.command({
+    command: 'add',
+    describe: 'Adda new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: true,
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        console.log('Adding a new note', argv.title, argv.body)
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler () {
+        console.log('Reading the note')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler () {
+        console.log('Listing out all notes')
+    }
+})
+
+yargs.parse()
